5b8ac912ff17ac5bc9ac564ab0b873bfa363a4d5
ErikGmz <erikgomez198755@outlook.com>
2024-01-29 19:20:01 -0600

    Created application helpers in order to improve the navbar view code.
    Co-authored-by: Luis Gutierrez <DevLuis123@users.noreply.github.com>
    Co-authored-by: Alan Hernández <BlazeDex@users.noreply.github.com>
    Co-authored-by: Loloxo <STR1ND3R@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_234612) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "date"
     t.string "location"
     t.float "price"
-    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
