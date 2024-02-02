6689213a48328b4050406eec8da43b01aa80f99d
ErikGmz <erikgomez198755@outlook.com>
2024-02-02 02:00:40 -0600

    Added events filtering by dates ranges and private/public option system.
    Co-authored-by: Luis Gutierrez <DevLuis123@users.noreply.github.com>
    Co-authored-by: Alan Hernández <BlazeDex@users.noreply.github.com>
    Co-authored-by: Loloxo <STR1ND3R@users.noreply.github.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,12 +14,14 @@ class Event < ApplicationRecord
   validates :price, presence: true
   validates :image, size: { less_than: 10.megabytes }, content_type: ['image/jpg', 'image/png', 'image/jpeg']
 
+  scope :all_user_events, ->(author_id) {
+    where("user_id = ?", author_id).or(where(public: true)).order(date: :desc, updated_at: :desc)
+  }
+  scope :only_private_events, -> { where(public: false) }
+  scope :events_between_dates, ->(start_date, end_date) { where(date: start_date..end_date) }
+
   def image_is_saved_and_exists?
     image_blob = image.blob
     image.attached? && image_blob.present? && image_blob.persisted?
-  end
-
-  def self.users_visible_events(author_id)
-    Event.where(user_id: author_id).or(Event.where(public: true))
   end
 end
