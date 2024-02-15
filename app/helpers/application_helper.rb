3f1b57ad3d5843081b7b9ac27465edc4519f0760
ErikGmz <erikgomez198755@outlook.com>
2024-02-15 03:08:40 -0600

    Major UI changes in events index and form views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,21 +8,21 @@ module ApplicationHelper
   end
 
   def print_get_started_link
-    link_to 'Get Started', events_path, class: 'no-underline ml-8 text-center text-gray-100 hover:text-gray-300 font-bold'
+    link_to 'Get Started', events_path, class: 'no-underline ml-6 sm:ml-8 text-center text-gray-100 hover:text-gray-300 font-bold'
   end
 
   def print_sign_in_link
-    link_to 'Sign in', new_user_session_path, class: 'no-underline ml-8 text-center text-gray-100 hover:text-gray-300 font-bold'
+    link_to 'Sign in', new_user_session_path, class: 'no-underline ml-6 sm:ml-8 text-center text-gray-100 hover:text-gray-300 font-bold'
   end
 
   def print_edit_profile_link
     link_to 'Edit Profile', edit_user_registration_path,
-            class: 'no-underline ml-8 text-center text-gray-100 hover:text-gray-300 font-bold'
+            class: 'no-underline ml-6 sm:ml-8 text-center text-gray-100 hover:text-gray-300 font-bold'
   end
 
   def print_sign_out_button
     button_to 'Sign out', destroy_user_session_path, method: :delete,
-                                                     class: 'ml-8 text-center text-gray-100 hover:text-gray-300 font-bold'
+                                                     class: 'ml-6 sm:ml-8 text-center text-gray-100 hover:text-gray-300 font-bold'
   end
 
   def print_username
